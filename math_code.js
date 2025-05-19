812c5b51f3fba816d09bc30255e029075fa54946
bethany <bethanycrist88@gmail.com>
2025-05-19 10:56:27 -0400

    fixed the array syntax ([#,#,#] to [#][#][#])

--- math_code.js
+++ math_code.js
@@ -100,146 +100,140 @@ function calculate(){
 
 function topLeftCornerNode(n,m){
     //console.log("left");
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,6] = fOld[n+1,m+1,6];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][6] = fOld[n+1][m+1][6];
 
-    fNew[n,m,1] = fNew[n,m,3];
-    fNew[n,m,4] = fNew[n,m,2];
-    fNew[n,m,8] = fNew[n,m,6];
+    fNew[n][m][1] = fNew[n][m][3];
+    fNew[n][m][4] = fNew[n][m][2];
+    fNew[n][m][8] = fNew[n][m][6];
 
-    rhoB = (rho[n+1,m] + rho[n,m+1])/2;
-    fNew[n,m,5] = (rhoB-fNew[n,m,0] - fNew[n,m,1]-fNew[n,m,2]-fNew[n,m,3]-fNew[n,m,4]-fNew[n,m,6]-fNew[n,m,8])/2;
-    fNew[n,m,7] = fNew[n,m,5];
+    rhoB = (rho[n+1][m] + rho[n][m+1])/2;
+    fNew[n][m][5] = (rhoB-fNew[n][m][0] - fNew[n][m][1]-fNew[n][m][2]-fNew[n][m][3]-fNew[n][m][4]-fNew[n][m][6]-fNew[n][m][8])/2;
+    fNew[n][m][7] = fNew[n][m][5];
 }
 
 function topRightCornerNode(n,m){
     //console.log("right")
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,5] = fOld[n+1,m-1,5];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][5] = fOld[n+1][m-1][5];
 
-    fNew[n,m,3] = fNew[n,m,1];
-    fNew[n,m,4] = fNew[n,m,2];
-    fNew[n,m,7] = fNew[n,m,5];
+    fNew[n][m][3] = fNew[n][m][1];
+    fNew[n][m][4] = fNew[n][m][2];
+    fNew[n][m][7] = fNew[n][m][5];
 
-    rhoB = (rho[n+1,m]+rho[n,m-1])/2;
-    fNew[n,m,6] = (rhoB-fNew[n,m,0] - fNew[n,m,1] - fNew[n,m,2] - fNew[n,m,3] - fNew[n,m,4] - fNew[n,m,5] - fNew[n,m,7])/2;
-    fNew[n,m,8] = fNew[n,m,6];
+    rhoB = (rho[n+1][m]+rho[n][m-1])/2;
+    fNew[n][m][6] = (rhoB-fNew[n][m][0] - fNew[n][m][1] - fNew[n][m][2] - fNew[n][m][3] - fNew[n][m][4] - fNew[n][m][5] - fNew[n][m][7])/2;
+    fNew[n][m][8] = fNew[n][m][6];
 }
 
 function otherTopNodes(n,m){
     //console.log("other");
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,5] = fOld[n+1,m-1,5];
-    fNew[n,m,6] = fOld[n+1,m+1,6];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][5] = fOld[n+1][m-1][5];
+    fNew[n][m][6] = fOld[n+1][m+1][6];
 
-    fNew[n,m,4] = fNew[n,m,2];
-    rhoB = fNew[n,m,0] + fNew[n,m,1] + fNew[n,m,3] + 2*(fNew[n,m,2] + fNew[n,m,5] + fNew[n,m,6]);
-    fNew[n,m,7] = fNew[n,m,5] + (fNew[n,m,1] - fNew[n,m,3])/2 - rhoB*uLid/2;
-    fNew[n,m,8] = fNew[n,m,6] + (fNew[n,m,3] - fNew[n,m,1])/2 + rhoB*uLid/2;
+    fNew[n][m][4] = fNew[n][m][2];
+    rhoB = fNew[n][m][0] + fNew[n][m][1] + fNew[n][m][3] + 2*(fNew[n][m][2] + fNew[n][m][5] + fNew[n][m][6]);
+    fNew[n][m][7] = fNew[n][m][5] + (fNew[n][m][1] - fNew[n][m][3])/2 - rhoB*uLid/2;
+    fNew[n][m][8] = fNew[n][m][6] + (fNew[n][m][3] - fNew[n][m][1])/2 + rhoB*uLid/2;
 }
 
 function bottomLeftCornerNode(n,m){
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,7] = fOld[n-1,m+1,7];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][7] = fOld[n-1][m+1][7];
 
-    fNew[n,m,1] = fNew[n,m,3];
-    fNew[n,m,2] = fNew[n,m,4];
-    fNew[n,m,5] = fNew[n,m,7];
-    rhoB = (rho[n-1,m] + rho[n,m+1])/2;
-    fNew[n,m,6] = (rhoB - fNew[n,m,0] - fNew[n,m,1] - fNew[n,m,2] - fNew[n,m,3] - fNew[n,m,4] - fNew[n,m,5] - fNew[n,m,7])/2;
-    fNew[n,m,8] = fNew[n,m,6];
+    fNew[n][m][1] = fNew[n][m][3];
+    fNew[n][m][2] = fNew[n][m][4];
+    fNew[n][m][5] = fNew[n][m][7];
+    rhoB = (rho[n-1][m] + rho[n][m+1])/2;
+    fNew[n][m][6] = (rhoB - fNew[n][m][0] - fNew[n][m][1] - fNew[n][m][2] - fNew[n][m][3] - fNew[n][m][4] - fNew[n][m][5] - fNew[n][m][7])/2;
+    fNew[n][m][8] = fNew[n][m][6];
 }
 
 function bottomRightCornerNode(n,m){
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,8] = fOld[n-1,m-1,8];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][8] = fOld[n-1][m-1][8];
 
-    fNew[n,m,3] = fNew[n,m,1];
-    fNew[n,m,2] = fNew[n,m,4];
-    fNew[n,m,6] = fNew[n,m,8];
-    rhoB = (rho[n-1,m] + rho[n,m-1])/2;
-    fNew[n,m,5] = (rhoB - fNew[n,m,0] - fNew[n,m,1] - fNew[n,m,2] - fNew[n,m,3] - fNew[n,m,4] - fNew[n,m,6] - fNew[n,m,8])/2;
-    fNew[n,m,7] =  fNew[n,m,5];
+    fNew[n][m][3] = fNew[n][m][1];
+    fNew[n][m][2] = fNew[n][m][4];
+    fNew[n][m][6] = fNew[n][m][8];
+    rhoB = (rho[n-1][m] + rho[n][m-1])/2;
+    fNew[n][m][5] = (rhoB - fNew[n][m][0] - fNew[n][m][1] - fNew[n][m][2] - fNew[n][m][3] - fNew[n][m][4] - fNew[n][m][6] - fNew[n][m][8])/2;
+    fNew[n][m][7] =  fNew[n][m][5];
 } 
 
 function otherBottomNodes(n,m){
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,7] = fOld[n-1,m+1,7];
-    fNew[n,m,8] = fOld[n-1,m-1,8];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][7] = fOld[n-1][m+1][7];
+    fNew[n][m][8] = fOld[n-1][m-1][8];
 
-    fNew[n,m,2] = fNew[n,m,4];
-    fNew[n,m,5] = fNew[n,m,7] + (fNew[n,m,3] - fNew[n,m,1])/2;
-    fNew[n,m,6] = fNew[n,m,8] + (fNew[n,m,1] - fNew[n,m,3])/2;
+    fNew[n][m][2] = fNew[n][m][4];
+    fNew[n][m][5] = fNew[n][m][7] + (fNew[n][m][3] - fNew[n][m][1])/2;
+    fNew[n][m][6] = fNew[n][m][8] + (fNew[n][m][1] - fNew[n][m][3])/2;
 }
 
 function leftBoundary(n,m){
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,6] = fOld[n+1,m+1,6];
-    fNew[n,m,7] = fOld[n-1,m+1,7];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][6] = fOld[n+1][m+1][6];
+    fNew[n][m][7] = fOld[n-1][m+1][7];
 
-    fNew[n,m,1] = fNew[n,m,3];
-    fNew[n,m,5] = fNew[n,m,7] + (fNew[n,m,4] - fNew[n,m,2])/2;
-    fNew[n,m,8] = fNew[n,m,6] + (fNew[n,m,2] - fNew[n,m,4])/2;
+    fNew[n][m][1] = fNew[n][m][3];
+    fNew[n][m][5] = fNew[n][m][7] + (fNew[n][m][4] - fNew[n][m][2])/2;
+    fNew[n][m][8] = fNew[n][m][6] + (fNew[n][m][2] - fNew[n][m][4])/2;
 }
 
 function rightBoundary(n,m){
-    fNew[n,m,0] = fOld[n,m,0]; 
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,5] = fOld[n+1,m-1,5];
-    fNew[n,m,8] = fOld[n-1,m-1,8];
+    fNew[n][m][0] = fOld[n][m][0]; 
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][5] = fOld[n+1][m-1][5];
+    fNew[n][m][8] = fOld[n-1][m-1][8];
 
-    fNew[n,m,3] = fNew[n,m,1];
-    fNew[n,m,6] = fNew[n,m,8] + (fNew[n,m,4] - fNew[n,m,2])/2;
-    fNew[n,m,7] = fNew[n,m,5] + (fNew[n,m,2] - fNew[n,m,4])/2;
+    fNew[n][m][3] = fNew[n][m][1];
+    fNew[n][m][6] = fNew[n][m][8] + (fNew[n][m][4] - fNew[n][m][2])/2;
+    fNew[n][m][7] = fNew[n][m][5] + (fNew[n][m][2] - fNew[n][m][4])/2;
 }
 
 function interiorNodes(n,m){
-    fNew[n,m,0] = fOld[n,m,0];
-    fNew[n,m,1] = fOld[n,m-1,1];
-    fNew[n,m,2] = fOld[n+1,m,2];
-    fNew[n,m,3] = fOld[n,m+1,3];
-    fNew[n,m,4] = fOld[n-1,m,4];
-    fNew[n,m,5] = fOld[n+1,m-1,5];
-    fNew[n,m,6] = fOld[n+1,m+1,6];
-    fNew[n,m,7] = fOld[n-1,m+1,7];
-    fNew[n,m,8] = fOld[n-1,m-1,8];
+    fNew[n][m][0] = fOld[n][m][0];
+    fNew[n][m][1] = fOld[n][m-1][1];
+    fNew[n][m][2] = fOld[n+1][m][2];
+    fNew[n][m][3] = fOld[n][m+1][3];
+    fNew[n][m][4] = fOld[n-1][m][4];
+    fNew[n][m][5] = fOld[n+1][m-1][5];
+    fNew[n][m][6] = fOld[n+1][m+1][6];
+    fNew[n][m][7] = fOld[n-1][m+1][7];
+    fNew[n][m][8] = fOld[n-1][m-1][8];
 }
 
 function momentCalculation(n,m){
     //console.log("m: " + m + ", n: " + n);
     //console.log(fNew[n][m]);
     rho[n][m] = sum(fNew[n][m].slice(0, 9));
-    u[n,m] = (fNew[n,m,1] + fNew[n,m,5] + fNew[n,m,8] - fNew[n,m,3] - fNew[n,m,6] - fNew[n,m,7])/rho[n,m];
-    v[n,m] = (fNew[n,m,2] + fNew[n,m,5] + fNew[n,m,6] - fNew[n,m,4] - fNew[n,m,7] - fNew[n,m,8])/rho[n,m];
+    u[n][m] = (fNew[n][m][1] + fNew[n][m][5] + fNew[n][m][8] - fNew[n][m][3] - fNew[n][m][6] - fNew[n][m][7])/rho[n][m];
+    v[n][m] = (fNew[n][m][2] + fNew[n][m][5] + fNew[n][m][6] - fNew[n][m][4] - fNew[n][m][7] - fNew[n][m][8])/rho[n][m];
 }
 
 function fEqCalculation(n,m){
 
 }
 
-//calculate();
-
-for (let m = 0; m < Nx; m++){
-    for (let n = 0; n < Ny; n++){
-        console.log(fNew[n,m]);
-    }
-}
+calculate();
